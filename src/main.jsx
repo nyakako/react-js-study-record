6a3eb43053b6dee3d3ac7c375b7990856cca3bce
nyakako <nyakako13@gmail.com>
2024-04-18 17:10:07 +0900

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { StudyRecord } from "./StudyRecord.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <StudyRecord />
+  </React.StrictMode>
+);
